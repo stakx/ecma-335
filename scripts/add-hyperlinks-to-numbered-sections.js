a077ace5c4ef1e82dff3acd53a9eaaa94fa79544
stakx <stakx@eml.cc>
2020-05-14 19:51:09 +0200

    Add script to replace missing hyperlinks for numbered sections

---
+++ scripts/add-hyperlinks-to-numbered-sections.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const todoMissingHyperlinkRegex = new RegExp(/\[([^\]]+)\]\(#todo-missing-hyperlink\)/gi);
+
+const docsDir = path.join(__dirname, '../docs/');
+const docs = fs.readdirSync(docsDir);
+for (const fileName of docs.filter(f => f.toLowerCase().endsWith('.md'))) {
+    const filePath = path.join(docsDir, fileName);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    console.log(fileName, ':');
+    const newFileContents = fileContents.replace(todoMissingHyperlinkRegex, (_, text, __, str) => {
+        const matchingFiles = docs.filter(f => f.toLowerCase().startsWith(text.toLowerCase() + '-'));
+        if (matchingFiles.length === 1) {
+            console.debug({ text, targetFileName: matchingFiles[0] });
+            return `[${text}](${matchingFiles[0]})`;
+        }
+        else {
+            return `[${text}](#todo-missing-hyperlink)`;
+        }
+    });
+    if (newFileContents != fileContents) {
+        fs.writeFileSync(filePath, newFileContents, { encoding: 'utf8', flag: 'w' });
+    }
+}
